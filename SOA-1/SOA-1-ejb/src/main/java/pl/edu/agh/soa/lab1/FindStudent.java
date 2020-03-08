f34479f4acdf310ff8eb777eeaafb454dc6a1d5f
Fajcon <kuba.ficon23@gmail.com>
2020-03-08 21:51:56 +0100

    add authorization

--- SOA-1/SOA-1-ejb/src/main/java/pl/edu/agh/soa/lab1/FindStudent.java
+++ SOA-1/SOA-1-ejb/src/main/java/pl/edu/agh/soa/lab1/FindStudent.java
@@ -10,6 +10,7 @@ import javax.jws.WebResult;
 import javax.jws.WebService;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Stateless
@@ -17,16 +18,11 @@ import java.util.List;
 public class FindStudent {
     @WebMethod(action = "find")
     @WebResult(name = "getStudent")
-    public Student find(@WebParam(name = "indexno") String no){
-        List<Student> students = new ArrayList<Student>();
-        Student x1 = new Student("Olek", "Czajka", "1234", Arrays.asList(new Course[] {new Course("SOA", 5)}));
-        Student x2 = new Student("Ala", "Sójka", "2345",  Arrays.asList ( new Course [] {new Course("SOA", 5), new
-                Course("SD", 10) }));
-        students.add(x1);
-        students.add(x2);
-        for(Student x:students){
-            if( x.getAlbum().equalsIgnoreCase(no) )
-            {
+    public Student find(@WebParam(name = "indexno") String no) {
+        List<Student> students = Arrays.asList(new Student("Olek", "Czajka", "1234", Collections.singletonList(new Course("SOA", 5))),
+                new Student("Ala", "Sójka", "2345", Arrays.asList(new Course("SOA", 5), new Course("SD", 10))));
+        for (Student x : students) {
+            if (x.getAlbum().equalsIgnoreCase(no)) {
                 return x;
             }
         }
--- SOA-1/SOA-1-ejb/src/main/java/pl/edu/agh/soa/lab1/Hello.java
+++ SOA-1/SOA-1-ejb/src/main/java/pl/edu/agh/soa/lab1/Hello.java
@@ -1,5 +1,8 @@
 package pl.edu.agh.soa.lab1;
 
+import org.jboss.annotation.security.SecurityDomain;
+import org.jboss.ws.api.annotation.WebContext;
+
 import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
@@ -7,14 +10,12 @@ import javax.jws.WebMethod;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import org.jboss.security.SecurityDomain;
 
 @Stateless
 @WebService
 @SecurityDomain("cxfservice-security-domain")
 @DeclareRoles({"TestRole"})
-@WebContext(contextRoot="/SOA-1", urlPattern="/*", authMethod="BASIC",
-        transportGuarantee="NONE", secureWSDLAccess = false)
+@WebContext(contextRoot="/SOA-1", urlPattern="/*", authMethod="BASIC",transportGuarantee="NONE", secureWSDLAccess = false)
 @SOAPBinding(style=SOAPBinding.Style.RPC)
 public class Hello {
     @RolesAllowed("TestRole")
