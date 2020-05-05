f8c804c9a933b89a34dc7b9d746177afb0cb0e5b
Fajcon <kuba.ficon23@gmail.com>
2020-05-05 20:41:59 +0200

    Add Swagger

--- SOA-1/rest-api/src/main/java/pl/edu/agh/soa/lab1/LegoSetApp.java
+++ SOA-1/rest-api/src/main/java/pl/edu/agh/soa/lab1/LegoSetApp.java
@@ -1,19 +1,35 @@
 package pl.edu.agh.soa.lab1;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
+import javax.servlet.ServletConfig;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
 import java.util.HashSet;
 import java.util.Set;
 
-@ApplicationPath("/")
-public class LegoSetApp extends Application {
-    public LegoSetApp(){
 
+@ApplicationPath("/api")
+public class LegoSetApp extends Application {
+    public LegoSetApp(@Context ServletConfig servletConfig) {
+        super();
+
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setTitle("Lego Rest Api");
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/SOA-1-web/api");
+        beanConfig.setResourcePackage(LegoSetController.class.getPackage().getName());
+        beanConfig.setScan(true);
     }
+
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> s = new HashSet<>();
-        s.add(Hello.class);
-        s.add(LegoSetController.class);
-        return s;
+        Set<Class<?>> resources = new HashSet<>();
+        resources.add(Hello.class);
+        resources.add(LegoSetController.class);
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        return resources;
     }
 }
--- SOA-1/rest-api/src/main/java/pl/edu/agh/soa/lab1/LegoSetController.java
+++ SOA-1/rest-api/src/main/java/pl/edu/agh/soa/lab1/LegoSetController.java
@@ -1,13 +1,21 @@
 package pl.edu.agh.soa.lab1;
 
-import javax.jws.WebParam;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Path("/LegoSet")
+@Api(value = "/LegoSet")
+@ApplicationScoped
 public class LegoSetController {
     List<LegoSet> legoSets = new ArrayList<LegoSet>();
     Long maxNumber = 1L;
@@ -25,14 +33,20 @@ public class LegoSetController {
     }
 
     @GET
-    @Path("/{setNumber}")
+    @Path("/{legoSetId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public LegoSet getLegoSet(@PathParam("setNumber") Long setNumber) throws Exception {
-        return legoSets
-                .stream()
-                .filter(l -> l.getLegoSetNumber().equals(setNumber))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("No LegoSet found with the setNumber: "+ setNumber));
+    public Response getLegoSet(@PathParam("legoSetId") Long setNumber) throws Exception {
+        System.out.println(legoSets.size());
+        LegoSet legoSet = legoSets
+                        .stream()
+                        .filter(l -> l.getLegoSetNumber().equals(setNumber))
+                        .findFirst()
+                        .orElseThrow(() -> new IllegalArgumentException("No LegoSet found with the setNumber: "+ setNumber));
+        return Response
+                .ok()
+                .entity(legoSet).header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                .build();
     }
 
     @POST
@@ -48,6 +62,8 @@ public class LegoSetController {
                 .boxGraphicBase64(boxGraphicBase64)
                 .legoPacks(Arrays.asList(new LegoPack(legoBlock, 5L), new LegoPack(legoBlock2, 4L)))
                 .build();
+        legoSets.add(legoSet);
+        System.out.println(legoSets.size());
         return maxNumber;
     }
 }
